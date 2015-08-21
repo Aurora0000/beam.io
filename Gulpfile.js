b43c7f66b43be2845a4b33e2fe0d7765e7ae8db1
Aurora0000 <aurora0000@users.noreply.github.com>
2015-08-21 15:08:55 +0100

    :arrow_up: electron-prebuilt v0.31.0

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,19 +2,31 @@ var gulp = require('gulp');
 var electron = require('gulp-atom-electron');
 
 gulp.task('package-linux-x64', function() {
-	return gulp.src('./**')
-		.pipe(electron({ version: '0.30.4', platform: 'linux', arch: 'x64'}))
-		.pipe(electron.zfsdest('release/beam-linux-x64.zip'));
+  return gulp.src('./**')
+    .pipe(electron({
+      version: '0.31.0',
+      platform: 'linux',
+      arch: 'x64',
+    }))
+    .pipe(electron.zfsdest('release/beam-linux-x64.zip'));
 });
 
 gulp.task('package-linux', function() {
-	return gulp.src('./**')
-		.pipe(electron({ version: '0.30.4', platform: 'linux', arch: 'ia32'}))
-		.pipe(electron.zfsdest('release/beam-linux-ia32.zip'));
+  return gulp.src('./**')
+    .pipe(electron({
+      version: '0.31.0',
+      platform: 'linux',
+      arch: 'ia32',
+    }))
+    .pipe(electron.zfsdest('release/beam-linux-ia32.zip'));
 });
 
 gulp.task('package-windows-x64', function() {
-	return gulp.src('./**')
-		.pipe(electron({ version: '0.30.4', platform: 'win32', arch: 'x64'}))
-		.pipe(electron.zfsdest('release/beam-win64.zip'));
+  return gulp.src('./**')
+    .pipe(electron({
+      version: '0.31.0',
+      platform: 'win32',
+      arch: 'x64',
+    }))
+    .pipe(electron.zfsdest('release/beam-win64.zip'));
 });
